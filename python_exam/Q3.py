f8f39997ff2f4d97ae2528b14000b4b5c7f4beaf
kshitizkool <87076914+kshitizkool@users.noreply.github.com>
2022-11-08 09:06:37 +0530

    Update Q3.py

--- python_exam/Q3.py
+++ python_exam/Q3.py
@@ -18,7 +18,29 @@ def number_of_routes(source_stopid: str, destination_stopid: str) -> int:
     """
     final_count = -1
     try:
-        # Enter your code here
+        file = open("C:/Users/GVS-ASUS/Downloads/anaheim_gtfs.zip", "r")
+        line = file.readline()
+
+        while(len(line) == True):    
+            l = line.split()
+            source_stopid = int(l[0])
+            destination_stopid = int(l[1])
+            src_node_cost = int(float(l[3]))
+
+            a = Q1_dijkstra(1,933)
+            print("THE SHORTEST PATH IS:", a)  
+
+        visited_node = [] 
+        initial_node_cost = float('inf')
+
+        for neighbour_node in a:
+                if neighbour_node not in visited_node:    
+
+                    visited_node.append[source_stopid] = [destination_stopid]
+                    new_node_cost = initial_node_cost + src_node_cost
+
+                    if(initial_node_cost[neighbour_node] > new_node_cost):
+                        file[neighbour_node] = new_node_cost    
         return final_count
     except:
         return final_count
